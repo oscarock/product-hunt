dfbcf65b245e225248a246da17857e005b0336dc
Oscar Diaz <o-pipe@hotmail.com>
2017-08-06 15:01:12 -0500

    crea la ruta /product/new y su formulario

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get '/products', to: 'products#index'
+  # get '/products', to: 'products#index'
+  # get '/products/new', to: 'products#new'
+  
+  root 'products#index'
+  resource :products
 
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
